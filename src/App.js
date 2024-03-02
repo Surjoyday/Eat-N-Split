cedc5597cf3156772353d4c7ca67e59370807506
Surjoyday Talukdar <90568406+Surjoyday@users.noreply.github.com>
2024-03-03 01:53:26 +0530

    Used key prop to reset state

--- src/App.js
+++ src/App.js
@@ -87,6 +87,7 @@ export default function App() {
         <SplitBillForm
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
         />
       )}
     </div>
